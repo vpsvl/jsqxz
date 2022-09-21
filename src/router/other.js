bb21cff4280121590edac55a721a6b0db504d955
shijie <shijie@stu.xmu.edu.cn>
2022-09-21 10:41:46 +0800

    update

--- src/router/other.js
+++ src/router/other.js
@@ -2,11 +2,19 @@ export default {
   path: '/other',
   component: () => import('@/views/layout/layout.vue'),
   name: 'other',
-  redirect: {name: 'otherMeridian'},
+  redirect: {name: 'otherKeymap'},
   meta: {
     title: '其他',
   },
   children: [
+    {
+      path: 'keymap',
+      component: () => import('@/views/keymap/index.vue'),
+      name: 'otherKeymap',
+      meta: {
+        title: '快捷键',
+      },
+    },
     {
       path: 'meridian',
       component: () => import('@/views/meridian/index.vue'),
---
+++ src/views/keymap/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-table class="v-table-keymap" :cols="thead" :data="tbody">
+    <template #keys="{row}">
+      <div class="td-block">
+        <p v-for="(name, val) of row.keys" :key="val">
+          <span class="keymap-key">{{ val }}：</span>
+          <span class="color-success">{{ name }}</span>
+        </p>
+      </div>
+    </template>
+  </v-table>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import data from '@/data/keymap/index';
+
+const thead = [
+  {
+    key: 'name',
+    name: '场景',
+  },
+  {
+    key: 'keys',
+    name: '快捷键',
+  },
+];
+const tbody = ref(data.list);
+</script>
+
+<style lang="less">
+.v-table-keymap {
+  .td:first-child {
+    flex: 0 0 150px;
+  }
+
+  .keymap-key {
+    display: inline-block;
+    width: 60px;
+    text-align: right;
+    color: var(--color-error);
+  }
+}
+</style>
