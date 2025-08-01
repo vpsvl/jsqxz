295f42dcf3d377d2f0902ce351492b914fa365a0
shijie <shijie@stu.xmu.edu.cn>
2025-08-02 01:55:15 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -28,15 +28,6 @@ export default defineConfig(({command}) => {
     },
     build: {
       outDir: 'docs',
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return id.toString().split('node_modules/')[1].split('/')[0].toString();
-            }
-          },
-        },
-      },
     },
   };
   if (command === 'build') {
