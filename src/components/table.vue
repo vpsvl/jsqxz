dbc9a90d05e69cb23458b038fd3fe2420df42981
shijie <shijie@stu.xmu.edu.cn>
2025-08-24 16:30:53 +0800

    update

--- src/components/table.vue
+++ src/components/table.vue
@@ -4,21 +4,32 @@
       <div class="td th-sort" v-for="th of columns" :key="th.key">
         <span>{{ th.name }}</span>
         <template v-if="th.sort">
-          <i class="sort-arrow-up" :class="{active: sortActive === th.key + '-1'}" @click="sort(th.key, -1)"></i>
-          <i class="sort-arrow-down" :class="{active: sortActive === th.key + 1}" @click="sort(th.key, 1)"></i>
+          <i
+            class="sort-arrow-up"
+            :class="{active: sortActive === th.key + '-1'}"
+            @click="sort(th.key, -1)"
+          ></i>
+          <i
+            class="sort-arrow-down"
+            :class="{active: sortActive === th.key + 1}"
+            @click="sort(th.key, 1)"
+          ></i>
         </template>
       </div>
     </div>
     <v-scroll class="tbody" v-if="data.length > 0">
-      <transition-group name="sort">
-        <div class="tr" v-for="(tr, index) of data" @click="clickTr(tr, index)" :key="tr.id ? tr.id : index">
-          <div class="td" v-for="td of columns" :key="td.key">
-            <slot :name="td.key" :row="tr" :index="index">
-              <p v-if="typeof tr[td.key] !== 'object'">{{ tr[td.key] }}</p>
-            </slot>
-          </div>
+      <div
+        class="tr"
+        v-for="(tr, index) of data"
+        @click="clickTr(tr, index)"
+        :key="tr.id ? tr.id : index"
+      >
+        <div class="td" v-for="td of columns" :key="td.key">
+          <slot :name="td.key" :row="tr" :index="index">
+            <p v-if="typeof tr[td.key] !== 'object'">{{ tr[td.key] }}</p>
+          </slot>
         </div>
-      </transition-group>
+      </div>
     </v-scroll>
     <div class="tr no-data" v-if="data.length <= 0">
       <div class="td">{{ state.loading ? '加载中，请稍候...' : '暂无更多数据' }}</div>
@@ -55,7 +66,7 @@ const props = defineProps({
 const emit = defineEmits(['tr-click', 'sort']);
 const state = inject('state');
 const sortActive = ref('');
-const columns = computed(() => props.cols.filter(item => !item.hidden))
+const columns = computed(() => props.cols.filter((item) => !item.hidden));
 
 onBeforeRouteLeave(() => {
   sortActive.value = '';
